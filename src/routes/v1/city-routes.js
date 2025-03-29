d77215de1ffd56355b8380484f2e58a9ca04a0f5
Rajveer Singh <pheonixrajveer@gmail.com>
2025-03-29 14:56:09 +0530

    Added create city api

---
+++ src/routes/v1/city-routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const { CityController } = require("../../controllers");
+const { CityMiddlewares } = require("../../middlewares");
+
+const router = express.Router();
+
+// /api/v1/city POST
+router.post(
+  "/",
+  CityMiddlewares.validateCreateRequestCity,
+  CityController.createCity,
+);
+
+module.exports = router;
