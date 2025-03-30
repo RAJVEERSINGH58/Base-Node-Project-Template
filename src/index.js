5dd45438ee4aca762ca7dd71c2114d929bbbed26
Rajveer Singh <pheonixrajveer@gmail.com>
2025-03-30 16:00:48 +0530

    Added new migration to add foreign key constraint in one to many relation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { ServerConfig , Logger } = require('./config');
 const apiRoutes = require('./routes');
+const { where } = require('sequelize');
 
 const app = express();
 
@@ -10,6 +11,24 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/api' , apiRoutes);
 
-app.listen(ServerConfig.PORT , ()=>{
+app.listen(ServerConfig.PORT , async ()=>{
   console.log("app is listening");
-})
+
+  const { City , Airport } = require('./models');
+  const New_Delhi = await City.findByPk(3);
+  console.log(New_Delhi);
+  // const airport = await Mumbai.create({name: 'Chhatrapati Shivaji Maharaj International Airport' , code: 'BOM' , city_Id: 2});
+  
+  // const airport = await New_Delhi.createAirport({name: 'Safdarjung Airport' , code: 'SDJ'});
+  // console.log(airport);
+
+  // const sfairport = await Airport.findByPk(3);
+  // console.log(sfairport);
+
+  await City.destroy({
+    where : {
+      id: 3
+    }
+  });
+});
+
--- src/models/airport.js
+++ src/models/airport.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.belongsTo(models.City , {
-        foreignKey: city_Id,
+        foreignKey: 'city_Id',
         onDelete: 'CASCADE',
         onUpdate: 'CASCADE'
 
